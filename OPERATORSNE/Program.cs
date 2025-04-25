0fe954eff06660a66978e2df142cf346e12143fd
kumar6646 <sai.penusoft@gmail.com>
2025-04-25 11:19:54 +0530

    Explained About Operators.

--- OPERATORSNE/Program.cs
+++ OPERATORSNE/Program.cs
@@ -9,6 +9,22 @@ namespace OPERATORSNE
     /*
      * Git Hub: repository where we have all our code.
      * Push the code to repository using visual studio
+     * 
+     * Operator: 
+     * Operator is used to operate between two operands.
+     * Ex: c=a+b;
+     * here a and b are called operands where "+" is an operator.
+     * Types of operator:
+     * 1)Aritmetic Operator
+     * 2)Assignment Operator
+     * 3)Increment and Decrement Operator
+     * 4)Relational Operator:
+     * 5)Bit wise Operator:
+     * 6)Conditional Operator:
+     * 
+     * 
+     * Aritmetic Operator: it used to perform mathematical operations.
+     * +,-,*,/,%
      */
     internal class Program
     {
